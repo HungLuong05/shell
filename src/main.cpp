b1e60cf3180fd5a8031c410198aae624ef11be9c
HungLuong05 <duchung210705@gmail.com>
2025-06-23 16:50:38 +0700

    migrate completion

--- src/main.cpp
+++ src/main.cpp
@@ -18,40 +18,9 @@
 #include "parser.hpp"
 #include "path.hpp"
 #include "builtins.hpp"
+#include "completion.hpp"
 
 int next_history_position = 0;
-
-char* command_generator(const char* text, int state) {
-  static size_t builtin_index = 0, path_index = 0;
-  static std::string current_text;
-
-  if (state == 0) {
-    builtin_index = 0;
-    path_index = 0;
-    current_text = text;
-  }
-
-  while (builtin_index < BUILTIN_COMMANDS.size()) {
-    const std::string& arg = BUILTIN_COMMANDS[builtin_index++];
-    if (arg.compare(0, current_text.size(), current_text) == 0) {
-      return strdup(arg.c_str());
-    }
-  }
-
-  while (path_index < PATH_COMMANDS.size()) {
-    const std::string& arg = PATH_COMMANDS[path_index++];
-    if (arg.compare(0, current_text.size(), current_text) == 0) {
-      return strdup(arg.c_str());
-    }
-  }
-
-  return nullptr;
-}
-
-char** command_completion(const char* text, int start, int end) {
-  rl_attempted_completion_over = 1;
-  return rl_completion_matches(text, command_generator);
-}
 
 void executeCommand(const Command& cmd) {
   int stdout_backup = -1, stderr_backup = -1;
---
+++ src/shell/completion.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+char* command_generator(const char* text, int state);
+char** command_completion(const char* text, int start, int end);
